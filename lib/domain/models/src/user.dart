bad166baa4d877ee08fadec05f4aa3d5a3839c9f
abiyyu19 <mhmdabiyyu19@gmail.com>
2025-09-19 10:42:32 +0700

    feat(model): create user model

---
+++ lib/domain/models/src/user.dart
@@ -0,0 +1,36 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:userapp/core/core.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+sealed class User with _$User {
+  @JsonSerializable(explicitToJson: true, includeIfNull: false, fieldRename: FieldRename.snake)
+  factory User({
+    required final String id,
+    required final String email,
+
+    // Will not null for auth user from Local DB
+    final String? username,
+    final String? firstName,
+    final String? lastName,
+    final String? avatar,
+  }) = _User;
+
+  factory User.fromJson(final JSON json) => _$UserFromJson(json);
+}
+
+/*
+User Response From Reqres
+
+{
+  "id": 1,
+  "email": "george.bluth@reqres.in",
+  "first_name": "George",
+  "last_name": "Bluth",
+  "avatar": "https://reqres.in/img/faces/1-image.jpg"
+}
+
+
+*/
